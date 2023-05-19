3bc208c3d220f966934a707a5e81fa31ec243ba8
BenjaminPontier <benjamin@limengo.com>
2023-05-19 10:38:26 +0200

    continue to work on tkinter and system !

--- main.py
+++ main.py
@@ -10,16 +10,83 @@ class Encryption:
                          'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.__key_value = {}
         self.__list = []
-        self.__word = input("Entrez un mot a chiffrer/déchiffrer:")
-        self.__word = self.__word.upper()
-        self.__word = self.__word.replace(" ", "")
 
     def get_key_value(self):
         return self.__key_value
 
-    def create_lines(self):
+    def create_lines(self, word):
+        pass
+
+    def read_lines(self):
+        file = open("data.txt", "r")
+        lines = file.readlines()
+        print(lines)
+        increment_number = 1
+        for line in lines:
+            print("line", increment_number, ":", line)
+            self.__key_value[increment_number] = line
+            self.__key_value[increment_number] = line.rstrip("\n")
+            increment_number += 1
+
+    def create_key(self, word):
+        descending_number = len(word)
+        list_temp = []
+
+        for i in range(len(word)):
+            list_temp.append(i)
+
+        for i in range(len(list_temp)):
+            random_element = random.choice(list_temp)
+            self.__list.append(random_element)
+            list_temp.remove(random_element)
+            descending_number -= 1
+
+    def verify_list(self):
+        for item in self.__list:
+            if self.__list.count(item) > 1:
+                return True
+        return False
+
+    def encryption(self, word):
+        result = [[]]
+        print(self.__key_value)
+        for x in range(len(self.__list)):
+            i = self.__list[x]
+            letter = word[x]
+            for j in range(len(self.__key_value[i])):
+                if self.__key_value[i][j] == letter:
+                    if j + 6 >= len(self.__key_value[i]):
+                        a = j + 6 - len(self.__key_value[i])
+                        new_letter = self.__key_value[i][a + 1 + 6]
+                    else:
+                        new_letter = self.__key_value[i][j + 6]
+                    result[x][j] = new_letter
+        return result
+
+    def decipher(self, word, key):
+        result = [[]]
+        for x in range(len(key)):
+            i = key[x]
+            letter = word[x]
+            for j in range(len(self.__key_value[i])):
+                if self.__key_value[i][j] == letter:
+                    result[x][j] = self.__key_value[i][j - 6]
+        return result
+
+class Console(Encryption):
+
+    def __init__(self):
+        super().__init__()
+        self.__response = input("Voulez vous chiffrez ou dechiffrez un mot ?")
+
+        if self.__response.lower() == "chiffrez":
+            self.encryption()
+        elif self.__response.lower() == "dechiffrez":
+            self.decipher()
+
+    def create_lines(self, word):
         self.__file = open("data.txt", "w")
-        for i in range(len(self.__word)):
+        for i in range(len(word)):
             descending_number = 26
             self.__dictio = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
                              'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -30,62 +97,40 @@ class Encryption:
                 descending_number -= 1
             self.__file.write("\n")
 
-    def read_lines(self):
-        self.__file = open("data.txt", "r")
-        lines = self.__file.readlines()
-        increment_number = 1
-        for line in lines:
-            self.__key_value[increment_number] = line
-            self.__key_value[increment_number] = line.rstrip("\n")
-            increment_number += 1
+    def decipher(self):
+        word = input("Donnez le mot a dechiffrer:")
+        key = input("Donnez la clef de dechiffrement:")
 
-        print(self.__key_value)
+        key.replace("[", "")
+        key.replace("]", "")
+        key = key.split(",")
+        keys = []
+        for i in range(len(key)):
+            keys.append(int(key[i]))
 
-    def create_list(self):
-        descending_number = len(self.__word)
-        list_temp = []
-
-        for i in range(len(self.__word)):
-            list_temp.append(i)
-
-        for i in range(len(list_temp)):
-            random_element = random.choice(list_temp)
-            self.__list.append(random_element)
-            list_temp.remove(random_element)
-            descending_number -= 1
-        print(self.__list)
-
-    def verify_list(self):
-        for item in self.__list:
-            if self.__list.count(item) > 1:
-                return True
-        return False
+        decipher = super().decipher(word, keys)
+        for i in range(len(decipher)):
+            for j in range(len(decipher[i])):
+                print(decipher[i][j], end="")
+            print("")
 
     def encryption(self):
-        for x in range(len(self.__list)):
-            i = self.__list[x]
-            letter = self.__word[x]
-            for j in range(len(self.__key_value[i])):
-                if self.__key_value[i][j] == letter:
-                    if j + 6 >= len(self.__key_value[i]):
-                        a = j + 6 - len(self.__key_value[i])
-                        new_letter = self.__key_value[i][a + 1 + 6]
-                    else:
-                        new_letter = self.__key_value[i][j + 6]
-                    print(new_letter)
 
-    def decipher(self, key):
-        for x in range(len(self.__list)):
-            i = key[x]
-            letter = self.__word[x]
-            for j in range(len(self.__key_value[i])):
-                if self.__key_value[i][j] == letter:
-                    new_letter = self.__key_value[i][j - 6]
-                    print(new_letter, end="")
+        result = input("Donnez un mot a chiffrer:")
+        self.create_lines(result)
+        super().read_lines()
+        super().create_key(result)
+        super().verify_list()
 
+        encryption = super().encryption(result)
+        for i in range(len(encryption)):
+            for j in range(len(encryption[i])):
+                print(encryption[i][j], end="")
+            print("")
 
-class Affichage:
+class View(Encryption):
     def __init__(self):
+        self.__encryption = super().__init__()
         self.__root = Tk()
         self.__button1 = Button(text="Encryption", command=self.info_encryption)
         self.__button1.config(width=30, height=5, bg="red")
@@ -123,13 +168,15 @@ class Affichage:
         self.__entry_key.pack()
         self.__label3 = Label(text="Mettez ici votre cylindre:")
         self.__label3.pack()
-        self.__entry_data = Entry(self.__root_info, width=50, bg="gray")
+        self.__entry_data = Entry(self.__root_info, width=50)
         self.__entry_data.pack()
         self.__send_button = Button(text="Envoyer les informations", command=self.decipher)
         self.__send_button.pack()
 
     def encryption(self):
         self.__word = self.__entry_word.get()
+        self.__word = self.__word.upper()
+        self.__word = self.__word.replace(" ", "")
 
         self.__root_info.destroy()
         self.__root_encryption = Tk()
@@ -140,22 +187,25 @@ class Affichage:
 
     def decipher(self):
         self.__word = self.__entry_word.get()
+        self.__word = self.__word.upper()
+        self.__word = self.__word.replace(" ", "")
         self.__key = self.__entry_key.get()
         self.__dict = self.__entry_data.get()
 
-        self.__root_info.destroy()
-        self.__root_decipher = Tk()
-        self.__root_decipher.title("Decipher")
-        self.__root_decipher.geometry("500x300")
 
-        self.__root_decipher.mainloop()
+        if len(self.__dict) == 0 or len(self.__dict) % 26 != 0:
+            self.__entry_data.config(bg="red")
+        else:
+            self.__root_info.destroy()
+            self.__root_decipher = Tk()
+            self.__root_decipher.title("Decipher")
+            self.__root_decipher.geometry("500x300")
+            super().decipher(self.__word, self.__key)
+
+            self.__root_decipher.mainloop()
 
 
-affichage = Affichage()
-# encryption = Encryption()
-# encryption.read_lines()
-# encryption.create_list()
-# encryption.verify_list()
-# encryption.decipher(
-#    [12, 16, 29, 6, 33, 9, 22, 15, 20, 3, 1, 30, 32, 36, 19, 10, 35, 27, 25, 26, 2, 18, 31, 14, 34, 17, 23, 7, 8, 21, 4,
-#     13, 11, 24, 28, 5])
+affichage = Console()
+
+
+#View()
