c2d3dab1642ccecc7e96ee74e7a656ca48b694c0
NewZAZ <ben.tintin.bp@gmail.com>
2023-05-21 19:07:53 +0200

    clean code and add sort !

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Samuel, Benjamin 1ARITH
 
-Ce programme est conçu pour effectuer des opérations arithmétiques simples.
+Ce programme permet de chiffrez ou déchiffrez des mots grace à un cylindre et une clé de chiffrement.
 
 ## Mode Console
 
-Pour lancer la version console, veuillez suivre ces étapes :
+Pour lancer la **version console**, veuillez suivre ces étapes :
 
 1. Assurez-vous que la __ligne 283__ du fichier soit décommentée.
 2. Exécutez le programme en appuyant sur la flèche verte dans PyCharm.
 
-![img.png](img.png)
+![img.png](img/img.png)
+
+### Fontionnement
+
+Suffit juste de lancer le programme et de suivre les instructions, **la clé de chiffrement est généré automatiquement**.
+Le cylindre aussi est généré automatiquement.
+**Sauf pour le déchiffrement**, il faut rentrer la clé de chiffrement et le cylindre (dans le fichier data.txt).
 
 ## Mode Tkinter
 
@@ -18,10 +24,16 @@ Pour lancer la version Tkinter, suivez ces étapes :
 1. Assurez-vous que la __ligne 285__ du fichier soit décommentée.
 2. Exécutez le programme en appuyant sur la flèche verte dans PyCharm.
 
-![img_1.png](img_1.png)
+![img_1.png](img/img_1.png)
 
 Une fois le mode de lancement choisi et les étapes appropriées suivies, le programme se lancera et vous pourrez commencer à l'utiliser.
 
 N'hésitez pas à appuyer sur la flèche verte dans PyCharm pour exécuter le programme.
 
-![img_2.png](img_2.png)
+![img_2.png](img/img_2.png)
+
+***Nous avons ajouter un triage pour mettre le mot dans sa case respectif***
+
+## Utilisation
+
+Si vous voulez changer le cylindre, vous devez modifier dans le fichier **data.txt**
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import random
 from tkinter import *
+from typing import IO
 
 
 class Encryption:
 
     def __init__(self):
-        self.__file = None
-        self.__dictio = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                         'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        self.__key_value = {}
-        self.__list = []
+        self.__file: IO = None
+        # The dictionary is used to create the lines
+        self.__dictio: list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                               'S',
+                               'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        # This list used for cylinder
+        self.__cylinder: dict = {}
+        # This list is used for the key
+        self.__keys: list = []
 
-    def get_key_value(self):
-        return self.__key_value
+    def get_cylinder(self):
+        return self.__cylinder
 
-    def set_key_value(self, key_value):
-        self.__key_value = key_value
+    def set_cylinder(self, key_value):
+        self.__cylinder = key_value
 
-    def get_list(self):
-        return self.__list
+    def get_keys(self):
+        return self.__keys
 
-    def create_lines(self, word):
+    def create_cylinder(self, word):
         self.__file = open("data.txt", "w")
         result = {}
         for i in range(len(word)):
@@ -39,16 +44,18 @@ class Encryption:
         self.__file.close()
         return result
 
-    def read_lines(self):
+    # This function is used to read the lines of the file
+    def read_cylinder(self):
         file = open("data.txt", "r")
         lines = file.readlines()
         increment_number = 1
         for line in lines:
-            self.__key_value[increment_number] = line
-            self.__key_value[increment_number] = line.rstrip("\n")
+            self.__cylinder[increment_number] = line
+            self.__cylinder[increment_number] = line.rstrip("\n")
             increment_number += 1
-        return self.__key_value
+        return self.__cylinder
 
+    # This function is used to create the key
     def create_key(self, word):
         descending_number = len(word)
         list_temp = []
@@ -58,44 +65,40 @@ class Encryption:
 
         for i in range(len(list_temp)):
             random_element = random.choice(list_temp)
-            self.__list.append(random_element)
+            self.__keys.append(random_element)
             list_temp.remove(random_element)
             descending_number -= 1
-        return self.__list
+        return self.__keys
 
-    def verify_list(self):
-        for item in self.__list:
-            if self.__list.count(item) > 1:
-                return True
-        return False
-
+    # This function is used to encrypt the word
     def encryption(self, word):
         result = [[]]
-        for x in range(len(self.__list)):
-            i = self.__list[x]
+        for x in range(len(self.__keys)):
+            i = self.__keys[x]
             letter = word[x]
             temp_list = []
-            for j in range(len(self.__key_value[i])):
-                if self.__key_value[i][j] == letter:
-                    if j + 6 >= len(self.__key_value[i]):
-                        a = j + 6 - len(self.__key_value[i])
-                        new_letter = self.__key_value[i][a + 1 + 6]
+            for j in range(len(self.__cylinder[i])):
+                if self.__cylinder[i][j] == letter:
+                    if j + 6 >= len(self.__cylinder[i]):
+                        a = j + 6 - len(self.__cylinder[i])
+                        new_letter = self.__cylinder[i][a + 1 + 6]
                     else:
-                        new_letter = self.__key_value[i][j + 6]
+                        new_letter = self.__cylinder[i][j + 6]
                     temp_list.append(new_letter)
             result.append(temp_list)
 
         return result
 
+    # This function is used to decipher the word
     def decipher(self, word, key):
         result = [[]]
         for x in range(len(key)):
             i = key[x]
             letter = word[x]
             temp_list = []
-            for j in range(len(self.__key_value[i])):
-                if self.__key_value[i][j] == letter:
-                    temp_list.append(self.__key_value[i][j - 6])
+            for j in range(len(self.__cylinder[i])):
+                if self.__cylinder[i][j] == letter:
+                    temp_list.append(self.__cylinder[i][j - 6])
             result.append(temp_list)
         return result
 
@@ -110,7 +113,10 @@ class Console:
             self.encryption()
         elif self.__response.lower() == "dechiffrer":
             self.decipher()
+        else:
+            print("Veuillez choisir entre chiffrer ou dechiffrer")
 
+    # This function is used to decipher the word by console input
     def decipher(self):
         word = input("Donnez le mot a dechiffrer:").upper()
         key = input("Donnez la clef de dechiffrement:")
@@ -127,51 +133,63 @@ class Console:
             for j in range(len(decipher[i])):
                 print(decipher[i][j], end="")
 
+    # This function is used to encrypt the word by console input
     def encryption(self):
 
         result = input("Donnez un mot a chiffrer:")
-        lines = self.__encryption.create_lines(result)
-        self.__encryption.set_key_value(lines)
+        lines = self.__encryption.create_cylinder(result)
+        self.__encryption.set_cylinder(lines)
         key = self.__encryption.create_key(result)
 
-        encryption = self.__encryption.encryption(result.upper())
-        for i in range(len(encryption)):
-            for j in range(len(encryption[i])):
-                print(encryption[i][j], end="")
+        resultEncryption = self.__encryption.encryption(result.upper())
+        for i in range(len(resultEncryption)):
+            for j in range(len(resultEncryption[i])):
+                print(resultEncryption[i][j], end="")
         print("\n", key, end="")
 
 
-
+# This class is used to create the GUI of the program and to interact with the user
 class View:
     def __init__(self, encryption):
+        self.__root_info = None
+        self.__cypher = None
+        self.__main_page = None
         self.__encryption = encryption
         self.init_main_page()
 
+    # This function is used to initialize the main page
     def init_main_page(self):
         self.__main_page = Tk()
         self.__main_page.title("Chiffrage | Déchiffrage")
         self.__main_page.geometry("500x300")
-        self.__main_page.config(bg="black")
-        Button(self.__main_page, text="Chiffrer", command=lambda: self.init_encryption_page(False), width=20, height=5, background="black", fg="red").pack(pady=20)
-        Button(self.__main_page, text="Déchiffrer", command=lambda: self.init_encryption_page(True),width=20, height=5, background="black", fg="red").pack(pady=10)
+        self.__main_page.config(bg="black", takefocus=True)
+        Button(self.__main_page, text="Chiffrer", command=lambda: self.init_encryption_page(False), width=20, height=5,
+               background="red", fg="black", activebackground="#C5272a", anchor="center").pack(pady=20)
+        Button(self.__main_page, text="Déchiffrer", command=lambda: self.init_encryption_page(True), width=20, height=5,
+               background="red", fg="black", activebackground="#C5272a", anchor="center").pack(pady=10)
         self.__main_page.mainloop()
 
-
-    def init_encryption_page(self, bool):
+    # This function is used to initialize the encryption page
+    def init_encryption_page(self, is_cypher):
+        self.__cypher = not is_cypher
         self.__main_page.destroy()
         self.__root_info = Tk()
         self.__root_info.title("Page des infos")
         self.__root_info.geometry("500x300")
         self.__root_info.configure(bg="black")
-        self.__label = Label(text="Mot à chiffrer/déchiffrer :", fg="red", bg="black")
+        if is_cypher:
+            self.__label = Label(text="Mot à déchiffrer :", height=4, fg="red", bg="black")
+        else:
+            self.__label = Label(text="Mot à chiffrer :", height=4, fg="red", bg="black")
         self.__label.pack(pady=10)
-        self.__entry_word = Entry(self.__root_info, fg="red", bg="black")
+        self.__entry_word = Entry(self.__root_info, fg="black", bg="red")
         self.__entry_word.pack(pady=10)
-        self.__send_button = Button(text="Valider", fg="red", bg="black", command=lambda: self.encryption(bool))
+        self.__send_button = Button(text="Valider", fg="red", bg="black", command=lambda: self.encryption(is_cypher))
         self.__send_button.pack()
 
         self.__root_info.mainloop()
 
+    # This function is used to create key buttons
     def encryption(self, bool):
         self.__word = self.__entry_word.get()
         self.__word = self.__word.upper()
@@ -186,24 +204,23 @@ class View:
         self.__frame = Frame(self.__root_encryption, width=1000, height=900, bg="black")
         self.__frame.pack(anchor="center")
         if bool:
-            self.__encryption.read_lines()
+            self.__encryption.read_cylinder()
         else:
-            lines = self.__encryption.create_lines(self.__word)
-            self.__encryption.set_key_value(lines)
+            lines = self.__encryption.create_cylinder(self.__word)
+            self.__encryption.set_cylinder(lines)
 
         self.update_encryption_with_button()
 
         self.__root_encryption.mainloop()
 
-
-
     def switch_keys(self, btn):
         btn.config(state='disabled')
-        self.__encryption.get_list().append(btn["text"])
+        self.__encryption.get_keys().append(btn["text"])
 
-        if len(self.__encryption.get_list()) == len(self.__encryption.get_key_value()):
+        if len(self.__encryption.get_keys()) == len(self.__encryption.get_cylinder()):
             self.init_last_page()
 
+    # This function is used to update the encryption page with the key buttons
     def init_last_page(self):
         self.__root_encryption.destroy()
         self.__root_last_page = Tk()
@@ -214,72 +231,93 @@ class View:
         self.__frame = Frame(self.__root_last_page, width=1000, height=900, bg="black")
         self.__frame.pack(anchor="center")
 
-        for i in range(len(self.__encryption.get_key_value())):
+        for i in range(len(self.__encryption.get_cylinder())):
             arrow_up = Button(self.__frame, text="⬆", fg="red", bg="black")
-            arrow_up.config(command=lambda a=self.__encryption.get_list()[i]: self.pop_key_value(a))
+            arrow_up.config(command=lambda a=self.__encryption.get_keys()[i]: self.pop_key_value(True, a))
             arrow_up.grid(column=i, row=27)
             arrow_down = Button(self.__frame, text="⬇", fg="red", bg="black")
-            arrow_down.config(command=lambda a=self.__encryption.get_list()[i]: self.remove_key_value(a))
+            arrow_down.config(command=lambda a=self.__encryption.get_keys()[i]: self.remove_key_value(True, a))
             arrow_down.grid(column=i, row=28)
 
         clearText = Label(self.__frame, text="⬅ Clear", fg="red", bg="black")
-        clearText.grid(column=len(self.__encryption.get_key_value()), row=10)
+        clearText.grid(column=len(self.__encryption.get_cylinder()), row=10)
 
         cipherText = Label(self.__frame, text="⬅ Cipher", fg="red", bg="black")
-        cipherText.grid(column=len(self.__encryption.get_key_value()), row=16)
+        cipherText.grid(column=len(self.__encryption.get_cylinder()), row=16)
 
+        self.sort_key_value()
         self.update_encryption()
 
         self.__root_last_page.mainloop()
 
+    # This function is used to sort key by input
+    def sort_key_value(self):
+        for i in range(len(self.__encryption.get_cylinder())):
+            while not self.check_good_letter_at_index(self.__encryption.get_keys()[i],
+                                                      self.__word[i]):
+                self.pop_key_value(False, self.__encryption.get_keys()[i])
+
+    # This function is used to check if cypher key is on the right place or decrypt key is on the right place
+    def check_good_letter_at_index(self, index, letter):
+        if self.__cypher:
+            return self.__encryption.get_cylinder()[index][10] == letter
+        else:
+            return self.__encryption.get_cylinder()[index][16] == letter
+
+    # This function is used to update the encryption page with the key buttons
     def update_encryption_with_button(self):
-        for i in range(len(self.__encryption.get_key_value())):
-            for j in range(len(self.__encryption.get_key_value()[i + 1])):
-                Label(self.__frame, text=self.__encryption.get_key_value()[i + 1][j], background="black",
+        for i in range(len(self.__encryption.get_cylinder())):
+            for j in range(len(self.__encryption.get_cylinder()[i + 1])):
+                Label(self.__frame, text=self.__encryption.get_cylinder()[i + 1][j], background="black",
                       fg="red").grid(column=i,
                                      row=j)
             btn = Button(self.__frame, text=i + 1)
-            btn.config(fg="red", bg="black", command=lambda a=btn: self.switch_keys(a))
+            btn.config(fg="red", bg="black", command=lambda btnCloned=btn: self.switch_keys(btnCloned))
             btn.grid(column=i, row=27)
 
+    # This function is used to update all the encryption page
     def update_encryption(self):
-        if len(self.__encryption.get_list()) == len(self.__encryption.get_key_value()):
-            for i in range(len(self.__encryption.get_list())):
-                a = int(self.__encryption.get_list()[i])
-                for j in range(len(self.__encryption.get_key_value()[a])):
-                    Label(self.__frame, text=self.__encryption.get_key_value()[a][j], background="black",
+        if len(self.__encryption.get_keys()) == len(self.__encryption.get_cylinder()):
+            for i in range(len(self.__encryption.get_keys())):
+                key = int(self.__encryption.get_keys()[i])
+                for j in range(len(self.__encryption.get_cylinder()[key])):
+                    Label(self.__frame, text=self.__encryption.get_cylinder()[key][j], background="black",
                           fg="red").grid(
                         column=i,
                         row=j)
         else:
-            for i in range(len(self.__encryption.get_key_value())):
-                for j in range(len(self.__encryption.get_key_value()[i + 1])):
-                    Label(self.__frame, text=self.__encryption.get_key_value()[i + 1][j], background="black",
+            for i in range(len(self.__encryption.get_cylinder())):
+                for j in range(len(self.__encryption.get_cylinder()[i + 1])):
+                    Label(self.__frame, text=self.__encryption.get_cylinder()[i + 1][j], background="black",
                           fg="red").grid(
                         column=i,
                         row=j)
 
-    def pop_key_value(self, index):
-        result = self.__encryption.get_key_value()[index]
+    # This function is used to pop the first letter of the key
+    def pop_key_value(self, update, index):
+        result = self.__encryption.get_cylinder()[index]
+        first_letter = result[0]
 
-        t = result[0]
-        self.__encryption.get_key_value()[index] = result[1:]
-        self.__encryption.get_key_value()[index] += t
+        self.__encryption.get_cylinder()[index] = result[1:]
+        self.__encryption.get_cylinder()[index] += first_letter
 
-        self.update_encryption()
+        if update:
+            self.update_encryption()
 
-    def remove_key_value(self, index):
-        result = self.__encryption.get_key_value()[index]
+    # This function is used to remove the last letter of the key
+    def remove_key_value(self, update, index):
+        result = self.__encryption.get_cylinder()[index]
 
-        t = result[len(result) - 1]
-        self.__encryption.get_key_value()[index] = result[:len(result) - 1]
-        self.__encryption.get_key_value()[index] = t + self.__encryption.get_key_value()[index]
+        last_letter = result[len(result) - 1]
+        self.__encryption.get_cylinder()[index] = result[:len(result) - 1]
+        self.__encryption.get_cylinder()[index] = last_letter + self.__encryption.get_cylinder()[index]
 
-        self.update_encryption()
+        if update:
+            self.update_encryption()
 
 
 encryption = Encryption()
 
-#Console(encryption)
+# Console(encryption)
 
 View(encryption)
